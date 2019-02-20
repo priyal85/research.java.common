229c86f74f2d7a463e81b86ae7cea1c4ff801a00
Priyal Samaratunga <priyal.samaratunga@cambio.lk>
2019-02-20 09:15:49 +0530

    Formated Thread Local

--- src/main/java/com/test/ThreadTest.java
+++ src/main/java/com/test/ThreadTest.java
@@ -2,45 +2,50 @@ package com.test;
 
 public class ThreadTest
 {
-public static void main(String[] args) {
+  public static void main(String[] args)
+  {
 
     Singleton instance = Singleton.getInstance();
-    
-    Thread t1 = new Thread(()->{
-    	instance.addValue("Hello 1");
-    	sleepForMilis(200);
-    	instance.addValue("Hello 2");
-    	sleepForMilis(500);
-    	instance.addValue("Hello 3");
-    	sleepForMilis(1000);
-    	instance.addValue("Hello 4");
-    	sleepForMilis(1500);
-    	instance.printValues();
+
+    Thread t1 = new Thread(() -> {
+      instance.addValue("Hello 1");
+      sleepForMilis(200);
+      instance.addValue("Hello 2");
+      sleepForMilis(500);
+      instance.addValue("Hello 3");
+      sleepForMilis(1000);
+      instance.addValue("Hello 4");
+      sleepForMilis(1500);
+      instance.printValues();
     }, "T1");
-    
-    Thread t2 = new Thread(()->{
-    	instance.addValue("Val 1");
-    	sleepForMilis(100);
-    	instance.addValue("Val 2");
-    	sleepForMilis(100);
-    	instance.addValue("val 3");
-    	sleepForMilis(100);
-    	instance.addValue("Val 4");
-    	sleepForMilis(100);
-    	instance.printValues();
+
+    Thread t2 = new Thread(() -> {
+      instance.addValue("Val 1");
+      sleepForMilis(100);
+      instance.addValue("Val 2");
+      sleepForMilis(100);
+      instance.addValue("val 3");
+      sleepForMilis(100);
+      instance.addValue("Val 4");
+      sleepForMilis(100);
+      instance.printValues();
     }, "T2");
     t1.start();
     t2.start();
     instance.printValues();
-  
-}
 
-private static void sleepForMilis(long milis) {
-	try {
-		Thread.sleep(milis);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
+  }
+
+  private static void sleepForMilis(long milis)
+  {
+    try
+    {
+      Thread.sleep(milis);
+    }
+    catch (InterruptedException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 }
